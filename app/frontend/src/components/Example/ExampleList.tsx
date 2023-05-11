0cd602e3cfc76eed5b555a11a273862d85855592
cassiejones444 <47842107+cassiejones444@users.noreply.github.com>
2023-05-12 08:27:28 +1000

    Update ExampleList.tsx
    Fixed one of the three example questions

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,8 +9,8 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What has been the profit or loss reported?",
+        value: "What has been the profit or losses repoorted?"
     },
     { text: "What happens in a performance review?", value: "What happens in a performance review?" },
     { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
